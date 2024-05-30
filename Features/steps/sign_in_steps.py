63e9fa6c16dfb3503c8f1630437514164c44e0f4
1bthomp <brianthompson080@gmail.com>
2024-05-29 22:17:05 -0400

    5th commit

--- Features/steps/sign_in_steps.py
+++ Features/steps/sign_in_steps.py
@@ -19,16 +19,16 @@ driver.maximize_window()
 def main_page (context):
 
 #Navigate to the URL
-    driver.get("https://soft.reelly.io/sign-in")
+    context.driver.get("https://soft.reelly.io/sign-in")
     sleep(4)
 
     #Search in user
-context.app.sign_in_page.send_username("Thompb155@gmail.com")
-#search_bar_1.clear()
+context.app.sign_in_page.send_username()
+#search_bar_1.clear("")
 #search_bar_1.send_keys("Thompb155@gmail.com")
 
 # Search password
-context.app.sign_in_page.send_password("@qqD9Kfkpymn5Fb")
+context.app.sign_in_page.send_password()
 #search_bar_2.clear()
 #search_bar_2.send_keys("@qqD9Kfkpymn5Fb")
 
@@ -46,25 +46,28 @@ def settings_subs_payment (context):
 @Then('Verify subscription, back button, and upgrade plan')
 def verification (context):
 
-#Click subscription
-    context.app.verify_page.click_subscription_button()
-    if check_1 == "Subscription & payments":
-        print("1st check passed")
-    else:
-        print("not verified")
+    #Click subscription
 
-    context.app.verify_page.verify_back_button()
-    if check_2 == "Back":
-        print("2nd check passed")
-    else:
-        print("not verified")
+     check_1 = context.app.verify_page.click_subscription_button()
+sleep(4)
 
-    context.app.verify_page.verify_upgrade()
-    if check_3 == "Upgrade plan":
-        print("3rd check passed")
-    else:
-        print("not verified")
+if check_1 == "Subscription & payments":
+    print("1st check passed")
+else:
+   print("not verified")
 
-    sleep(4)
+check_2 = context.app.verify_page.verify_back_button()
+
+if check_2 == "Back":
+    print("2nd check passed")
+else:
+   print("not verified")
+
+check_3 = context.app.verify_page.verify_upgrade()
+
+if check_3 == "Upgrade plan":
+    print("3rd check passed")
+else:
+    print("not verified")
 
 driver.quit()
